724a1e03bb20ccc789c88292e7a0bf715cfb8969
JohnO <>
2021-05-22 23:04:57 +0300

    feat: Exercise-3: implement findLexicographicallyMinimalForStrings function

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,31 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+	NSMutableString *result = [[NSMutableString alloc] initWithCapacity:string1.length + string2.length];
+	NSUInteger i = 0;
+	NSUInteger j = 0;
+	BOOL endOfString1, endOfString2;
+	do {
+		endOfString1 = i >= string1.length;
+		endOfString2 = j >= string2.length;
+		if (!endOfString1 && !endOfString2) {
+			if ([string1 characterAtIndex:i] <= [string2 characterAtIndex:j]) {
+				[result appendString:[string1 substringWithRange:NSMakeRange(i, 1)]];
+				i++;
+			} else {
+				[result appendString:[string2 substringWithRange:NSMakeRange(j, 1)]];
+				j++;
+			}
+		} else if (!endOfString1) {
+			[result appendString:[string1 substringFromIndex:i]];
+			break;
+		} else if (!endOfString2) {
+			[result appendString:[string2 substringFromIndex:j]];
+			break;
+		}
+	} while (!endOfString1 || !endOfString2);
+	
+    return [result copy];
 }
 
 @end
