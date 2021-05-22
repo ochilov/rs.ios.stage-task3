5161f3446d1cdbe40f1a75ba7ed3934da6fbc77d
JohnO <>
2021-05-22 22:27:10 +0300

    feat: Exercise-2: implement dividePlayersIntoTeams function

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,22 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+	NSMutableArray *commands = [NSMutableArray new];
+	for (int i = 0, count = (int)ratingArray.count; i < count-2; ++i) {
+		NSInteger iVal = ratingArray[i].integerValue;
+		for (int j = i+1; j < count-1; ++j) {
+			NSInteger jVal = ratingArray[j].integerValue;
+			for (int k = j+1; k < count; ++k) {
+				NSInteger kVal = ratingArray[k].integerValue;
+				if ((iVal < jVal && jVal < kVal) // ascending ordered
+				 || (iVal > jVal && jVal > kVal) // descending ordered
+				) {
+					[commands addObject:@[ratingArray[i], ratingArray[j], ratingArray[k]]];
+				}
+			}
+		}
+	}
+    return commands.count;
 }
 
 @end
