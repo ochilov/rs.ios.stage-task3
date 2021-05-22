33d1f42014c7723be0c725fb42997667d026bc60
JohnO <>
2021-05-22 20:51:11 +0300

    feat: Exercise-1: implement DateHelper methods

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,24 +5,49 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+	NSDateFormatter *dateFormatter = [NSDateFormatter new];
+	if (0 < monthNumber && monthNumber <= dateFormatter.monthSymbols.count) {
+		return dateFormatter.monthSymbols[monthNumber-1];
+	}
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+	NSDateFormatter *dateFormatter = [NSDateFormatter new];
+	dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+	NSDate *dateVal = [dateFormatter dateFromString:date];
+	NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+	NSInteger day = [gregorianCalendar component:NSCalendarUnitDay fromDate:dateVal];
+    return day;
 }
 
 #pragma mark - Third
 
-- (NSString *)getDayName:(NSDate*) date {
-    return nil;
+- (NSString *)getDayName:(NSDate*) date {	
+	NSDateFormatter *dateFormatter = [NSDateFormatter new];
+	dateFormatter.dateFormat = @"E";
+	dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+	return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+	NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+
+	NSDateComponents *inputDateComponents = [gregorianCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitWeekOfMonth fromDate:date];
+	
+	NSDateComponents *currentDateComponents = [gregorianCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitWeekOfMonth fromDate:[NSDate date]];
+	
+	if (inputDateComponents .year == currentDateComponents .year &&
+		inputDateComponents.month == currentDateComponents.month &&
+		inputDateComponents.weekOfMonth == currentDateComponents.weekOfMonth
+	) {
+		return YES;
+	}
+	
     return NO;
 }
 
