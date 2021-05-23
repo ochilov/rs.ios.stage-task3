9b910fdcfc1441c17da6eb9383c04d971911b909
JohnO <>
2021-05-23 14:20:40 +0300

    feat: Exercise-4: implement LevelOrderTraversalForTree function

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,30 @@
 #import "LevelOrderTraversal.h"
 
+NSUInteger LevelOrderTraversalForTreeNodes(NSArray *tree, NSMutableArray<NSMutableArray *> *levelOrderedData, NSUInteger level, NSUInteger startIndex) {
+	
+	typedef enum {
+		LeftNode,
+		RightNode
+	} NodeType;
+	
+	NSUInteger index = startIndex;
+	for (NodeType nodeType = LeftNode; nodeType <= RightNode && index < tree.count; ++nodeType) {
+		NSObject *node = tree[index++];
+		if ([node isKindOfClass:NSNumber.class]) {
+			if (levelOrderedData.count <= level) {
+				[levelOrderedData addObject:[NSMutableArray new]];
+			}
+			[levelOrderedData[level] addObject:node];
+		 
+			index = LevelOrderTraversalForTreeNodes(tree, levelOrderedData, level+1, index);
+		}
+	}
+	return index;
+}
+
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+	NSMutableArray<NSMutableArray *> *levelOrderedData = [NSMutableArray new];
+	NSUInteger level = 0;
+	LevelOrderTraversalForTreeNodes(tree, levelOrderedData, level, 0);
+	return [levelOrderedData copy];
 }
